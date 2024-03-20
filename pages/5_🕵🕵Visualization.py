e51bbe8ab41cd4cca3610e61730938e22c5dad20
nadarktong <neangez@gmail.com>
2024-03-20 22:24:16 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -1,34 +1,30 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
 
 # ตัวอย่างข้อมูล
 #data = pd.DataFrame({
-#    "Country": ["ภาคเหนือ", "ภาคตะวันออก", "ภาคกลาง", "ภาคตะวันตก", "ภาคใต้"],
-#    "Population": [10000, 10000, 10000, 10000],
-#    "GDP": [50000,10000 , 15000, 500000]
+ #   "Country": ["ภาคเหนือ", "ภาคตะวันออก", "ภาคกลาง", "ภาคตะวันตก", "ภาคใต้"],
+ #  "Population": [10000, 10000, 10000, 10000],
+ #   "GDP": [50000,10000 , 15000, 500000]
 #})
 
-import matplotlib.pyplot as plt
 # แสดงชื่อกราฟ
-#st.title("ข้อมูลประเทศ")
+st.title("ข้อมูลประเทศ")
 
 # เลือกประเภทกราฟ
 chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar", "pie", "line"])
 
 # แสดงกราฟ
 if chart_type == "bar":
-    data = df.copy()
-    st.bar_chart(data, x="workclass", y="age")
+    st.bar_chart(data, x="workclass", y="age")
 elif chart_type == "pie":
-    column = "workclass"
-    value_counts = df[column].value_counts()
-    fig, ax = plt.subplots(figsize=(10, 10))
-    explode = (0,1,0,0)
-    ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150)
-    plt.title("Pie Chart of " + column)
-    st.pyplot(fig)
+    column = "workclass"
+    value_counts = df[column].value_counts()
+    fig, ax = plt.subplots(figsize=(10, 10))
+    explode = (0,1,0,0)
+    ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150, data=data)
+    plt.title("Pie Chart of " + column)
+    st.pyplot(fig)
 elif chart_type == "line":
-    st.line_chart(
-  df, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"]  # Optional
-)
+    st.line_chart(data, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"])
