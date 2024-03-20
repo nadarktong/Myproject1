ca497c5920aab0355b60f2da59512fd460472872
nadarktong <neangez@gmail.com>
2024-03-20 11:40:52 +0700

    Update 3_🕵🕵DataAndStat.py

--- pages/3_🕵🕵DataAndStat.py
+++ pages/3_🕵🕵DataAndStat.py
@@ -9,17 +9,11 @@ st.write(df.head(10))
 st.header("Show Chart")
 
 st.line_chart(
-   df, x=["published_at", "updated_at"], y="author", color=["#FF0000", "#0000FF"]  # Optional
+   df, x="author", y=["published_at", "updated_at"], color=["#FF0000", "#0000FF"]  # Optional
 )
 st.write(df.head(200))
 
-import matplotlib.pyplot as plt
-
-data = {'Category': ['A', 'B', 'C', 'D'],
-        'Values': [25, 18, 32, 22]}
-
-plt.bar(data['Category'], data['Values'])
-plt.xlabel('Category')
-plt.ylabel('Values')
+plt.xlabel('author')
+plt.ylabel('published_at','updated_at')
 plt.title('Bar Graph')
 plt.show()
