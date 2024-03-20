9db23a885d0563fd86148e49cd9085f820ee5c44
nadarktong <neangez@gmail.com>
2024-03-20 12:50:50 +0700

    Update 3_🕵🕵DataAndStat.py

--- pages/3_🕵🕵DataAndStat.py
+++ pages/3_🕵🕵DataAndStat.py
@@ -9,10 +9,12 @@ st.write(df.head(10))
 st.header("Show Chart")
 
 st.line_chart(
-  df, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"]  # Optional
+  df, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF", "#00FF00"]  # Optional
 )
 
 st.write(df.head(200))
 
-chart_type == "bar"
+chart_type = "bar"
+data = df.copy()
+
 st.bar_chart(data, x="age", y="workclass")
