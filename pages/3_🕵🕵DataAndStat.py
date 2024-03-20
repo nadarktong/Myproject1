4a7f9aaa35453ae665858b49d410bd3c8128596e
nadarktong <neangez@gmail.com>
2024-03-20 12:12:49 +0700

    .

--- pages/3_🕵🕵DataAndStat.py
+++ pages/3_🕵🕵DataAndStat.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.header("Show Data")
 
-df=pd.read_csv("./data/Most Popular Naruto Characters.csv")
+df=pd.read_csv("./data/adultSalary.csv")
 st.write(df.head(10))
 
 st.header("Show Chart")
@@ -11,4 +11,5 @@ st.header("Show Chart")
 #st.line_chart(
 #  df, x="author", y=["published_at", "updated_at"], color=["#FF0000", "#0000FF"]  # Optional
 #)
+
 st.write(df.head(200))
