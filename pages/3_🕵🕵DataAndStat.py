1d38c7cf1e281ce96f5f3748433748b4ce1b14e6
nadarktong <neangez@gmail.com>
2024-03-20 20:54:20 +0700

    Update 3_🕵🕵DataAndStat.py

--- pages/3_🕵🕵DataAndStat.py
+++ pages/3_🕵🕵DataAndStat.py
@@ -39,3 +39,31 @@ plt.title("Pie Chart of " + column)
 
 # แสดงกราฟบน Streamlit
 st.pyplot(fig)
+
+
+
+# เลือกคอลัมน์ที่ต้องการแสดง
+columns = ["age", "education"]
+
+# กำหนดค่า x
+x = df[column]
+
+# กำหนดค่า y 
+y_list = [df[column] for column in columns]
+
+# วาดกราฟเส้น
+for y in y_list:
+    plt.plot(x, y, label=column)
+
+# เพิ่มชื่อกราฟ
+plt.title("Line Chart of Multiple Columns")
+
+# เพิ่ม labels แกน x และ y
+plt.xlabel("Age")
+plt.ylabel("Salary")
+
+# เพิ่ม legend
+plt.legend()
+
+# แสดงกราฟบน Streamlit
+st.pyplot(fig)
