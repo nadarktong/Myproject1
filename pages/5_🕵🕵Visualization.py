7d36e60fbf5aa5edb18fb2c9acfe5b0c6873aa5e
nadarktong <neangez@gmail.com>
2024-03-20 22:58:03 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -17,15 +17,32 @@ chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar
 # แสดงกราฟ
 if chart_type == "bar":
     st.bar_chart(data, x="workclass", y="age")
-if chart_type == "pie":
-    column = "workclass"
+elif chart_type == "pie":
 
-    if not df["workclass"].empty:
-        value_counts = pd.Categorical(df[column]).value_counts()
-        fig, ax = plt.subplots(figsize=(10, 10))
-        explode = (0, 1, 0, 0)  # Adjust slice explosion if needed
-        ax.pie(value_counts, labels=value_counts.index, startangle=150, autopct=lambda pct: f'{pct:.1f}%')  # Format percentage string (optional)
-        plt.title("Pie Chart of " + column)
-        st.pyplot(fig)
-    else:
-        st.write
+    # เลือกคอลัมน์และจำนวนข้อมูล
+    column = "workclass"
+    value_counts = df[column].value_counts()
+
+    # กำหนดขนาดกราฟ
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    # วาดแผนภูมิวงกลม
+    explode = (0, 1, 0, 0)
+    ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150, data=data)
+
+    # ตั้งชื่อกราฟ
+    plt.title("Pie Chart of " + column)
+
+    # แสดงกราฟ
+    plt.show()
+
+elif chart_type == "line":
+
+    # วาดแผนภูมิเส้น
+    sns.lineplot(data=df, x="age", y=["workclass", "education"], color=["#FF0000", "#0000FF"])
+
+    # ตั้งชื่อกราฟ
+    plt.title("Line Chart of workclass and education vs age")
+
+    # แสดงกราฟ
+    plt.show()
