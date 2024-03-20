65bea179e787da52a937de1475428380841ea409
nadarktong <neangez@gmail.com>
2024-03-20 22:08:39 +0700

    Update 3_🕵🕵DataAndStat.py

--- pages/3_🕵🕵DataAndStat.py
+++ pages/3_🕵🕵DataAndStat.py
@@ -27,7 +27,7 @@ column = "workclass"
 value_counts = df[column].value_counts()
 fig, ax = plt.subplots(figsize=(10, 10))
 explode = (0,1,0,0)
-ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150)
+ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%',shadow=True, startangle=150)
 plt.title("Pie Chart of " + column)
 st.pyplot(fig)
 
