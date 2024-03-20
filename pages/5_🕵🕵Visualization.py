03930529f0e4377049d429c66652956c9bb06925
nadarktong <neangez@gmail.com>
2024-03-20 22:46:01 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -3,28 +3,27 @@ import pandas as pd
 import numpy as np
 
 # ตัวอย่างข้อมูล
-#data = pd.DataFrame({
- #   "Country": ["ภาคเหนือ", "ภาคตะวันออก", "ภาคกลาง", "ภาคตะวันตก", "ภาคใต้"],
- #  "Population": [10000, 10000, 10000, 10000],
- #   "GDP": [50000,10000 , 15000, 500000]
-#})
+data = pd.DataFrame({
+"workclass": ["Private", "Private", "Private", "Government", "Private"],
+"age": [30, 35, 40, 45, 50]
+})
 
 # แสดงชื่อกราฟ
-st.title("แสดงข้อมูล")
+st.title("กราฟแท่งแสดงอายุเฉลี่ยในแต่ละประเภทอาชีพ")
 
 # เลือกประเภทกราฟ
 chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar", "pie", "line"])
 
 # แสดงกราฟ
-chart_type == "bar"
-st.bar_chart(data, x="workclass", y="age") 
-chart_type == "pie"
-column = "workclass"
-value_counts = df[column].value_counts()
+if chart_type == "bar":
+# แสดงกราฟแท่ง
+st.bar_chart(data, x="workclass", y="age")
+
+elif chart_type == "pie": column = "workclass" value_counts = df[column].value_counts()
 fig, ax = plt.subplots(figsize=(10, 10))
 explode = (0,1,0,0)
 ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150, data=data)
 plt.title("Pie Chart of " + column)
 st.pyplot(fig)
-chart_type == "line"
+elif chart_type == "line":
 st.line_chart(data, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"])
