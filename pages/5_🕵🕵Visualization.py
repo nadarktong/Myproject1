37f850a3bdd5406c1ea044c4079ed98c78ae90f3
nadarktong <neangez@gmail.com>
2024-03-20 22:18:30 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -3,35 +3,32 @@ import pandas as pd
 
 
 # ตัวอย่างข้อมูล
-data = pd.DataFrame({
-    "Country": ["ภาคเหนือ", "ภาคตะวันออก", "ภาคกลาง", "ภาคตะวันตก", "ภาคใต้"],
-    "Population": [10000, 10000, 10000, 10000],
-    "GDP": [50000,10000 , 15000, 500000]
-})
+#data = pd.DataFrame({
+#    "Country": ["ภาคเหนือ", "ภาคตะวันออก", "ภาคกลาง", "ภาคตะวันตก", "ภาคใต้"],
+#    "Population": [10000, 10000, 10000, 10000],
+#    "GDP": [50000,10000 , 15000, 500000]
+#})
 
 import matplotlib.pyplot as plt
 # แสดงชื่อกราฟ
-st.title("ข้อมูลประเทศ")
+#st.title("ข้อมูลประเทศ")
 
 # เลือกประเภทกราฟ
-chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar", "pie", "line", "scatter"])
+chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar", "pie", "line"])
 
 # แสดงกราฟ
 if chart_type == "bar":
-    st.bar_chart(data, x="Country", y="Population")
+    data = df.copy()
+    st.bar_chart(data, x="workclass", y="age")
 elif chart_type == "pie":
-    labels = "ไทย", "อเมริกา", "จีน", "ญี่ปุ่น"
-    sizes =  [70000000, 330000000, 1400000000, 125000000]
-    explode = (0, 0.1,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-    st.pyplot(fig1)
+    column = "workclass"
+    value_counts = df[column].value_counts()
+    fig, ax = plt.subplots(figsize=(10, 10))
+    explode = (0,1,0,0)
+    ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150)
+    plt.title("Pie Chart of " + column)
+    st.pyplot(fig)
 elif chart_type == "line":
-    st.line_chart(data, x="Country", y="Population")
-
-
-# แสดงข้อมูล GDP
-st.write("ข้อมูล GDP")
-st.table(data[["Country", "GDP"]])
+    st.line_chart(
+  df, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"]  # Optional
+)
