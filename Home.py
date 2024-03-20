c172f24b6bdc6557e2aecb1055d20bbe24ac5265
nadarktong <neangez@gmail.com>
2024-03-20 23:07:47 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,11 +13,9 @@ def load_lottieurl(url: str):
 lottie_url_hello = "https://lottie.host/fcd01209-0ad6-4615-8477-c2d5f76624dd/fDSfJEABkm.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
-# Set the background color - this should be in the Streamlit app's config file, not in the code itself
-# st.markdown("""<style>body {background-color: #000;}</style>""", unsafe_allow_html=True)
 
-st.header("☢ อัตราป่วยตาย ด้วยโรคหลอดเลือดสมองตีบหรืออุดตันของโรงพยาบาลที่ให้บริการ 🕵")
-st.header("by Varut Pongsai")
+st.header("☢ ข้อมูลการสำรวจสำมะโนประชากร 🕵")
+st.header("by Varut")
 
 # Display the lottie animation
 st_lottie(lottie_hello, key="hello")
