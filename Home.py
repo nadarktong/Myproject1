3093ae2cb9f87b8668f8fae8c789655755071b1c
nadarktong <neangez@gmail.com>
2024-03-20 10:09:28 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,10 +1,8 @@
 import json
-import time 
+import time
 import requests
-
 import streamlit as st
-from streamlit_lottie import st_lottie #lottieใช้สำหรับรันไฟล์
-from streamlit_lottie import st_lottie_spinner
+from streamlit_lottie import st_lottie, st_lottie_spinner
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -12,13 +10,15 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-body style="background-color: black;"
 lottie_url_hello = "https://lottie.host/fcd01209-0ad6-4615-8477-c2d5f76624dd/fDSfJEABkm.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
-st.header("☢ อัตราป่วยตาย ด้วยโรคหลอดเลือดสมองตีบหรืออุดตัน ของโรงพยาบาลที่ให้บริการ 🕵")
+# Set the background color - this should be in the Streamlit app's config file, not in the code itself
+# st.markdown("""<style>body {background-color: #000;}</style>""", unsafe_allow_html=True)
+
+st.header("☢ อัตราป่วยตาย ด้วยโรคหลอดเลือดสมองตีบหรืออุดตันของโรงพยาบาลที่ให้บริการ 🕵")
 st.header("by Varut Pongsai")
 
-#การเรียกใช้งาาน lottie file
+# Display the lottie animation
 st_lottie(lottie_hello, key="hello")
 st.balloons()
