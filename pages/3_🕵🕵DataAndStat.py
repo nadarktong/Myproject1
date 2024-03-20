efb1974ae08c72d10d89bdd1121741bdb71d83a1
nadarktong <neangez@gmail.com>
2024-03-20 20:45:55 +0700

    Update 3_🕵🕵DataAndStat.py

--- pages/3_🕵🕵DataAndStat.py
+++ pages/3_🕵🕵DataAndStat.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
 
 st.header("Show Data")
 
@@ -10,31 +9,33 @@ st.write(df.head(10))
 st.header("Show Chart")
 
 st.line_chart(
-  df, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"]  # Optional
+  df, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"]  # Optional
 )
 
 st.write(df.head(200))
 
-# แก้ไข: เปลี่ยน 'filtered_df' เป็นชื่อ DataFrame ที่ต้องการใช้
+chart_type = "bar"
+data = df.copy()
+
+st.bar_chart(data, x="workclass", y="age")
+
 chart = alt.Chart(df).mark_bar().encode(
-    x=alt.X('age', title='Age'),
-    y=alt.Y('count()', title='Count'),  # ใช้ count() เพื่อนับจำนวนข้อมูลที่มีในแต่ละช่วงอายุ
-    color='workclass:N',
-    tooltip=['workclass', 'age']  # แก้ไขเป็นชื่อฟิลด์ที่ต้องการแสดงใน tooltip
+    x=alt.X('age', title='Age'),
+    y=alt.Y('count()', title='Count'),  # ใช้ count() เพื่อนับจำนวนข้อมูลที่มีในแต่ละช่วงอายุ
+    color='workclass:N',
+    tooltip=['workclass', 'age']  # แก้ไขเป็นชื่อฟิลด์ที่ต้องการแสดงใน tooltip
 ).properties(
-    width=600,
-    height=400,
-    title='Age Distribution by Platform'
+    width=600,
+    height=400,
+    title='Age Distribution by Platform'
 ).configure_title(
-    fontSize=20,
-    fontWeight='bold',
-    color='gray'
+    fontSize=20,
+    fontWeight='bold',
+    color='gray'
 ).configure_axis(
-    labelFontSize=12,
-    titleFontSize=16,
-    titleFontWeight='normal'
+    labelFontSize=12,
+    titleFontSize=16,
+    titleFontWeight='normal'
 )
 
 st.altair_chart(chart, use_container_width=True)
-
-
