433eff4b0cbdc55f599f18ae83b0d6b67130cf8a
nadarktong <neangez@gmail.com>
2024-03-21 20:50:58 +0700

    Update 2_✍✍Theory.py

--- pages/2_✍✍Theory.py
+++ pages/2_✍✍Theory.py
@@ -6,7 +6,7 @@ from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 
 def load_lottieurl(url: str):
-    r = requests.get(url)
+    r = requests.get(url) 
     if r.status_code != 200:
         return None
     return r.json()
