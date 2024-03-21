4c55a126d185c00dbcddd51f429ea4972e42f406
nadarktong <neangez@gmail.com>
2024-03-22 05:10:11 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -20,11 +20,11 @@ st.bar_chart(data, x="workclass", y="age")
 chart_type == "pie"
 column = "workclass"
 df = df.reset_index()
- value_counts = df['column'].value_counts()
- fig, ax = plt.subplots(figsize=(10, 10))
- explode = (0,1,0,0)
- ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150, data=data)
- plt.title("Pie Chart of " + column)
- st.pyplot(fig)
+value_counts = df['column'].value_counts()
+fig, ax = plt.subplots(figsize=(10, 10))
+explode = (0,1,0,0)
+ax.pie(value_counts, labels=value_counts.index, autopct='%1.2f%%', startangle=150, data=data)
+plt.title("Pie Chart of " + column)
+st.pyplot(fig)
 chart_type == "line"
 st.line_chart(data, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"])
