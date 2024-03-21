32ca717cf5d85aa75c191c0826ea0af464eb4912
nadarktong <neangez@gmail.com>
2024-03-22 05:08:43 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -16,10 +16,10 @@ chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar
 
 # แสดงกราฟ
 chart_type == "bar"
- st.bar_chart(data, x="workclass", y="age")
+st.bar_chart(data, x="workclass", y="age")
 chart_type == "pie"
- column = "workclass"
- df = df.reset_index()
+column = "workclass"
+df = df.reset_index()
  value_counts = df['column'].value_counts()
  fig, ax = plt.subplots(figsize=(10, 10))
  explode = (0,1,0,0)
@@ -27,4 +27,4 @@ chart_type == "pie"
  plt.title("Pie Chart of " + column)
  st.pyplot(fig)
 chart_type == "line"
- st.line_chart(data, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"])
+st.line_chart(data, x="age", y=["workclass","education"], color=["#FF0000", "#0000FF"])
